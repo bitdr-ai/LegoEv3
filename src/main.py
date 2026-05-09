3611694f9bcf426e72d178da5325f89c02a3e85c
bitdr-ai <carlossv2202@icloud.com>
2026-05-08 19:51:07 -0600

    refactored code

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@
 from robot.core import Robot
 from pybricks.parameters import Port, Color
 from pybricks.tools import wait
-import math
 
 robot = Robot()
 
-robot.ev3.speaker.beep()
-robot.Infrared_scan()
+
 
